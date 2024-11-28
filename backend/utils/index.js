00739094861a1299db2168bc017b61dc7b445618
saqibmir580-png <saqibmir580@gmail.com>
2024-11-28 11:05:19 -0800

    updated production

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -2,17 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
-import path from "path";
+import connectDB from "./db.js";
+import userRoute from "../routes/user.route.js";
+import companyRoute from "../routes/company.route.js";
+import jobRoute from "../routes/job.route.js";
+import applicationRoute from "../routes/application.route.js";
 
 dotenv.config({});
 
 const app = express();
-const _dirname=path.resolve()
 
 // middleware
 app.use(express.json());
@@ -34,10 +32,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")))
-app.get('*',(_,res)=>{
-    res.sendFile(path.resolve(_dirname,"frotend","dist","index.html"))
-})
+
 
 app.listen(PORT,()=>{
     connectDB();
